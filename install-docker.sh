2ffd3ff0aa1b08c495a92fb51637552990f50d97
Nischal Gautam <nischal1024@gmail.com>
2018-08-23 21:02:56 +1000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -6,3 +6,5 @@ apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 apt-get install nginx
 apt-get install docker-compose
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
